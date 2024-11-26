bcc0ae83315bec17631929ceb4342e8a318d607c
aBeeHivee <abihanaqvi511@gmail.com>
2024-11-26 22:39:16 +0530

    Changes in home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ResumeState from './Context/ResumeState';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
         </Routes>
       </div>
     </ResumeState>
